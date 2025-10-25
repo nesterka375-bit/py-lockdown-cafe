3d6847d084d49e26bfbafc69a1111755c8688b7a
Siarhei Nestsiaronak <nesterka375@gmail.com>
2025-10-25 12:15:03 +0200

    solved py-lockdown-cafe

---
+++ app/errors.py
@@ -0,0 +1,26 @@
+from itertools import count
+
+
+class VaccineError(Exception):
+    pass
+
+
+class NotVaccinatedError(VaccineError):
+    pass
+
+
+class OutdatedVaccineError(VaccineError):
+    def __init__(
+            self,
+            message: str = "All friends should be vaccinated"
+    ) -> None:
+        super().__init__(message)
+
+
+class NotWearingMaskError(Exception):
+    def __init__(
+            self,
+            message: str = "Friends should buy masks"
+    ) -> None:
+        super().__init__(message)
+        self.count = count
--- app/main.py
+++ app/main.py
@@ -1,1 +1,22 @@
-# write your code here
+import datetime
+from app.cafe import Cafe
+
+
+def go_to_cafe(friends: list, cafe: Cafe) -> str:
+
+    for friend in friends:
+        if (
+                not friend.get("vaccine")
+                or friend.get("vaccine").get("expiration_date")
+                < datetime.date.today()
+        ):
+            return "All friends should be vaccinated"
+
+    count = 0
+    for friend in friends:
+        if not friend.get("wearing_a_mask"):
+            count += 1
+    if count > 0:
+        return f"Friends should buy {count} masks"
+
+    return f"Friends can go to {cafe.name}"
